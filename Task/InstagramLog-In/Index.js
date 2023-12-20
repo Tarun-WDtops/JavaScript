716d367396fea63824c2ee207be8b200f22f8dd5
Tarun-WDtops <tjparmar786@gmail.com>
2023-12-20 15:50:52 +0530

    added js

--- Task/InstagramLog-In/Index.js
+++
@@ -1,7 +0,0 @@
-function army() {
-  let: document.getElementById("tarun").value;
-
-  if (type == pasword) {
-    ("password == text");
-  } else (type == text)("text == password");
-}
---
+++ Task/InstagramLog-In/java.js
@@ -0,0 +1,12 @@
+function inputPass() {
+  document.getElementById("show").style.display = "block";
+}
+
+function showPass() {
+  document.getElementById("show");
+
+  if (type == "password") {
+    password == "text";
+    document.getElementById("show").innerText = "show";
+  } else type == "text";
+}
