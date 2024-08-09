ce3d3e916865ad1891d3e6fb4fee855c23739951
Tarun-WDtops <tjparmar786@gmail.com>
2024-08-09 13:06:12 +0530

    js added

--- Practice/object/demo.js
+++ Practice/object/demo.js
@@ -13,13 +13,13 @@ const address = {
   },
 };
 function getAddressdata() {
-  console.log("id-->", address.fname);
-  console.log("id-->", address.lname);
-  console.log("id-->", address.telephone);
-  console.log("id-->", address.street);
-  console.log("id-->", address.City);
-  console.log("id-->", address.state);
-  console.log("id-->", address.country);
+  // console.log("id-->", address.fname);
+  // console.log("id-->", address.lname);
+  // console.log("id-->", address.telephone);
+  // console.log("id-->", address.street);
+  // console.log("id-->", address.City);
+  // console.log("id-->", address.state);
+  // console.log("id-->", address.country);
 }
 getAddressdata();
 // address.fname = "shankar";
@@ -27,3 +27,13 @@ getAddressdata();
 // console.log(address.street[2]);
 // console.log(address.fname);
 // console.log(address.getFullname());
+
+var a = 10;
+let b = "Tarun";
+var c = null;
+var d = undefined;
+function reAssign() {
+  let b = "this";
+  console.log(b);
+}
+console.log(b);
